ee04ceae43a1df650dc8423cb4443684ccfe11dd
pjh5365 <pjh5365@naver.com>
2023-07-27 13:36:58 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    
    - 스프링 웹
    - 엑추에이터
    - 데브툴즈
    - 롬복

---
+++ src/main/java/pjh5365/board/FastcampusBoardApplication.java
@@ -0,0 +1,13 @@
+package pjh5365.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampusBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pjh5365/board/FastcampusBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package pjh5365.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
