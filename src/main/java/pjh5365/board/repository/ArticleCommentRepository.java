4dd1b4b8acd182dc3403c37d5edc54e6999652a2
pjh5365 <pjh5365@naver.com>
2023-07-28 12:32:22 +0900

    #1 - data rest 설정
    게시글, 댓글 json api 를 자동으로 restful 하게 만들게 설정

--- src/main/java/pjh5365/board/repository/ArticleCommentRepository.java
+++ src/main/java/pjh5365/board/repository/ArticleCommentRepository.java
@@ -1,7 +1,9 @@
 package pjh5365.board.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import pjh5365.board.domain.ArticleComment;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
