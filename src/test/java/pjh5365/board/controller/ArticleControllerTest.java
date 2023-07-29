2912ab7d3e24610ccf4e50b268a26e358f65cfd3
pjh5365 <pjh5365@naver.com>
2023-07-29 14:27:18 +0900

    #16 - 게시글 관련 뷰 테스트 작성
    어떤 경러로 뷰를 열어줄지 테스트로 명세를 뽑음

---
+++ src/test/java/pjh5365/board/controller/ArticleControllerTest.java
@@ -0,0 +1,68 @@
+package pjh5365.board.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[View, GET] 게시글 리스트 (게시판) 페이지 - 정상호출")
+    @Test
+    void givenNoting_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("[View, GET] 게시글 상세 페이지 - 정상호출")
+    @Test
+    void givenNoting_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+    @DisplayName("[View, GET] 게시글 검섹 전용 페이지 - 정상호출")
+    @Test
+    void givenNoting_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @DisplayName("[View, GET] 게시글 해시태그 페이지 - 정상호출")
+    @Test
+    void givenNoting_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+}
