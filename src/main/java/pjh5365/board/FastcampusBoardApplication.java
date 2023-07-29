6213290eb15822218a85f285f2bebf19615aeda8
pjh5365 <pjh5365@naver.com>
2023-07-29 16:13:38 +0900

    #16 - Thymeleaf 3 decoupled logic 설정
    사용자 application property 로 제어할 수 있게 설정 추가

--- src/main/java/pjh5365/board/FastcampusBoardApplication.java
+++ src/main/java/pjh5365/board/FastcampusBoardApplication.java
@@ -2,7 +2,9 @@ package pjh5365.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class FastcampusBoardApplication {
 
